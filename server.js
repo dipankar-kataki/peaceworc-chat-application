06df14bb9aae00a13f9ee001342cdfee69340b8e
dipankar kataki <dipankar.kataki@ekodus.com>
2023-07-04 13:03:55 +0530

    sigin event socket

--- server.js
+++ server.js
@@ -4,32 +4,35 @@ const http = require('http').createServer(app)
 
 const PORT = process.env.PORT || 3000
 
+// app.use(express.static(__dirname + '/public'))
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html')
+// })
+
 http.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
 
-app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
 
 // Socket 
 const io = require('socket.io')(http)
+let clients = {};
 
 io.sockets.on('connection', (socket) => {
     console.log('Connected... To Websocket')
-    console.log('Socket Id', socket.id)
-    // socket.on('message', (msg) => {
-    //     socket.broadcast.emit('message', msg)
-    // })
-    
-    // socket.emit('welcome', data)
+    socket.on('signin', function(id){
+        clients[id] = socket;
+        console.log('Clients ==> ', clients)
+
+    })
+
+    console.log('======================= ********************* ===================================')
 
     //Sending Message
-
     socket.on('sendMessage', function(msg){
-        // let messageForSending = JSON.parse(msg);
+        let targetId = msg.targetId;
         let data = {
             chatResponse:{
                 image:msg.image,
@@ -39,26 +42,10 @@ io.sockets.on('connection', (socket) => {
             }
             
         }
-        // console.log(data)
-        socket.broadcast.emit('receiveMessage', data)
+        if(clients[targetId]){
+            clients[targetId].emit('receiveMessage', data)
+        }
     })
-
-    // socket.on('chat', function(response) {
-    //     console.log('subscribe trigged')
-    //     const room_data = response
-    //     console.log('Response From Android ==>',room_data)
-    // })
 })
-
-
-
-
-// client-side
-// io.on("connect", () => {
-//     // socket.broadcast.emit('test-msg', hello)
-   
-//     io.emit('test-msg','Free portobond')
-// });
-
 
   
